da6d7cb61967b59b06ff9ae0aa15f19db7c34196
LegendaryiDoom <alanortegonc@hotmail.com>
2019-03-29 12:04:25 -0600

    Prueba1

---
+++ Utilerias/BaseDatos.php
@@ -0,0 +1,30 @@
+<?php
+//---------------------------------------------------------------------
+// Utilerias de Bases de Dato
+// Alejandro Guzmán Zazueta
+// Enero 2019
+//---------------------------------------------------------------------
+
+try{
+        $Cn = new PDO('pgsql:host=localhost;port=5432;dbname=ProyectoX;user=postgres;password=hola');
+        //$Cn = new PDO('mysql:host=localhost; dbname=bdalumnos','root','');
+        $Cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $Cn->exec("SET CLIENT_ENCODING TO 'UTF8';");
+        //$Cn->exec("SET CHARACTER SET utf8");
+}catch(Exception $e){
+    die("Error: " . $e->GetMessage());
+}
+
+// Función para ejecutar consultas SELECT
+function Consulta($query)
+{
+    global $Cn;
+    try{    
+        $result =$Cn->query($query);
+        $resultado = $result->fetchAll(PDO::FETCH_ASSOC); 
+        $result->closeCursor();
+        return $resultado;
+    }catch(Exception $e){
+        die("Error en la LIN: " . $e->getLine() . ", MSG: " . $e->GetMessage());
+    }
+}
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+   
+</head>
+
+<body style="background-color:#D0F6A0;">
+
+<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+    <li><a href="#!">Iniciar Sesion/Registrarse</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">Mi cuenta</a></li>
+  </ul>
+  <ul id="dropdown2" class="dropdown-content">
+    <li><a href="#!">Primario</a></li>
+    <li><a href="#!">Secundario</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">Terciario</a></li>
+  </ul>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Inver-X</a>
+      <ul class="right hide-on-med-and-down">
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown2">Empresas<i class=
+      "material-icons right"></i></a></li>
+     
+        <!-- Dropdown Trigger -->
+        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Perfil<i class=
+        "material-icons right"></i></a></li>
+      </ul>
+    </div>
+  </nav>
+  
+
+       
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script type="text/javascript" src="./js/jquery-3.0.0.min.js"></script>
+  <script type="text/javascript" src="./js/materialize.min.js"></script>
+  
+  <script type="text/javascript" src="./js/jquery.validate.min.js"></script>     
+  <script type="text/javascript" src="./resources/js/carga.js">
+  </script> 
+  
+</body>
+<div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="./images/emprendedores.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+  </div>
+
+</html>
